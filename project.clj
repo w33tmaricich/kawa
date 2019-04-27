2b69b44d084a0a6c8323efb69e3b563e8922e2bf
Alexander Maricich <amaricich@skylinenet.net>
2019-04-27 01:08:03 -0400

    chore(project.clj): updated description
    - Updated the description and included url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kawa "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Wrapper for ffmpeg command line tools."
+  :url "https://github.com/w33tmaricich/kawa"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
