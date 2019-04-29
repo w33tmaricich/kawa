dd836acb9be250a332044260c408366b8e569435
Alexander Maricich <amaricich@skylinenet.net>
2019-04-29 12:56:09 -0400

    feat(core): sane ffmpeg flags
    - Added some aliases for ffmpeg flags that are not as verbose
      as they could have been.
    - Made some modifications and everything is up and running great!

--- src/kawa/core.clj
+++ src/kawa/core.clj
@@ -2,9 +2,68 @@
   (:use [clojure.java.shell :only [sh]]))
 
 (def FFMPEG {:cmd "ffmpeg"
-             :flags {}})
+             :flags {
+                     :audio-frames :aframes
+                     :audio-sampling-frequency :ar
+                     :audio-quality :aq
+                     :audio-sync :async
+                     :aspect-ratio :aspect
+                     :audio-codec :acodec
+                     :audio-sample-format :sample_fmt
+                     :audio-filtergraph :af
+                     :audio-force-tag :atag
+                     :copy-timebase :copytb
+                     :calculate-psnr :psnr
+                     :duration :t
+                     :disable-audio :an
+                     :disable-video :vn
+                     :data-frames :dframes
+                     :dump-payload :hex
+                     :disable-subtitles :sn
+                     :enable-interaction :stdin
+                     :frame-rate :r
+                     :frame-size :s
+                     :format :f
+                     :file-size-limit :fs
+                     :hardware-acceleration :hwaccel
+                     :hardware-acceleration-device :hwaccel_device
+                     :hardware-accelleration-list :hwaccels
+                     :intra-dc-precision :dc
+                     :initial-demux-decode-delay :muxpreload
+                     :input-url :i
+                     :input-time-offset :itsoffset
+                     :interlacing :ilme
+                     :maximum-demux-decode-delay :muxdelay
+                     :never-overwrite-output :n
+                     :no-filter :dn
+                     :no-auto-selection :dn
+                     :no-auto-map :dn
+                     :overwrite-output :y
+                     :preset :pre
+                     :print-timestamps :debug_ts
+                     :pixel-format :pix_fmt
+                     :quality-scale :q
+                     :rate-control-override :rc_override
+                     :read-at-native-framerate :re
+                     :rescale-input-timestamps :itsscale
+                     :subtitle-codec :scodec
+                     :subtitle-offset :fix_sub_duration
+                     :stop-at :to
+                     :seek-to :ss
+                     :seek-to-from-eof :sseof
+                     :show-license :L
+                     :show-bitstream-filters :bsfs
+                     :swscaler-flags :sws_flags
+                     :show-qp-histogram :qphist
+                     :subtitle-canvas-size :canvas_size
+                     :set-bitstream-filters :bsf
+                     :stop-on-error :xerror
+                     :video-frames :vframes
+                     :video-filtergraph :vf
+                     :video-sync :vsync
+                     }})
 
-(def FFPLAY {:cmd"ffplay"
+(def FFPLAY {:cmd "ffplay"
              :flags {:disable-audio :an
                      :disable-subtitles :sn
                      :disable-video :vn
@@ -19,6 +78,9 @@
                      :seek :ss
                      :window-x-pos :left
                      :window-y-pos :top}})
+
+(def FFPROBE {:cmd "ffprobe"
+              :flags {}})
 
 (defn fmt-cmd
   "Creates a vector that is ready to be passed to sh.
@@ -45,7 +107,10 @@
     out))
 
 (defn ffmpeg! [& args]
-  (sh-apply! FFMPEG args))
+  (apply sh-apply! FFMPEG args))
 
 (defn ffplay! [& args]
-  (sh-apply! FFPLAY args))
+  (apply sh-apply! FFPLAY args))
+
+(defn ffprobe! [& args]
+  (apply sh-apply! FFPROBE args))
--- test/kawa/core_test.clj
+++ test/kawa/core_test.clj
@@ -2,14 +2,16 @@
   (:require [clojure.test :refer :all]
             [kawa.core :refer :all]))
 
+;(ffplay! :duration 11 "rtsp://admin:robot@172.28.137.102:554/media/video1")
+
 (deftest formatting
-  (testing "format ffmpeg command"
+  (testing "format commands based off apps"
     (is (= (fmt-cmd FFPLAY :disable-audio)
            ["ffplay" "-an"]))
     (is (= (fmt-cmd FFMPEG :this "is" :very 2)
            ["ffmpeg" "-this" "is" "-very" "2"]))))
 
-;(deftest ffplay
-  ;(testing "playing a video"
-    ;(is (= 0
-           ;(ffplay! :disable-video :disable-audio :no-display :duration 10 "rtsp://admin:robot@172.28.137.102:554/media/video1")))))
+(deftest ffplay
+  (testing "playing a video"
+    (is (= "\n"
+           (ffplay! :autoexit :duration 10 "rtsp://admin:robot@172.28.137.102:554/media/video1")))))
